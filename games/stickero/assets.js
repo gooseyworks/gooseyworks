993acd48554a8ad1b2ac30af55bf4ae4f0ead827
gooseyworks <nathanjchanson@gmail.com>
2026-08-22 10:20:27 -0500

    Add asset loading for game sprites

---
+++ games/stickero/assets.js
@@ -0,0 +1,8 @@
+loadSprite("main", "assets/sprites/main.png");
+loadSprite("evil", "assets/sprites/evil.png");
+loadSprite("flag", "assets/sprites/flag.png");
+loadSprite("box", "assets/sprites/box.png");
+loadSprite("boss", "assets/sprites/boss.png");
+loadSprite("plate", "assets/sprites/plate.png");
+loadSprite("End", "assets/sprites/End.png");
+loadSprite("no", "assets/sprites/no.png");
